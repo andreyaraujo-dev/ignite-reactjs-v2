45c7ac2755a901d1250af6c85f5105256787698f
Andrey Araujo <andreyaraujo.dev@gmail.com>
2023-06-14 07:39:33 -0300

    feat: adiciona estilizacao na sidebar e header

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,22 @@
-import { Header } from './components/Header';
-
+import { Header } from './components/Header'
+import styles from './App.module.css'
 import './global.css'
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Andrey Araújo" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet risus at risus tempor rhoncus nec nec odio. Aliquam ut tempor risus, lacinia faucibus orci. Nulla mollis nisl vel tellus dignissim, vitae efficitur purus hendrerit. Etiam eu purus ultrices, consequat mauris a, commodo nibh." />
+          <Post author="Seu João" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet risus at risus tempor rhoncus nec nec odio. Aliquam ut tempor risus, lacinia faucibus orci. Nulla mollis nisl vel tellus dignissim, vitae efficitur purus hendrerit. Etiam eu purus ultrices, consequat mauris a, commodo nibh." />
+        </main>
+      </div>
+    </div>
   )
 }
 
